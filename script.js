8a3c10c562a00cbf04d108e69095001551a47a7d
laerkehansen <laha0007@stud.kea.dk>
2024-10-21 12:08:58 +0200

    alt jeg har lavet indtil nu

---
+++ script.js
@@ -0,0 +1,29 @@
+"use strict";
+
+let myArray = [];
+
+
+//henter alle mine elementer//
+const button = document.querySelector("button");
+const input = document.querySelector("input");
+const container = document.getElementById("list-container");
+
+const ul = document.createElement("ul"); //laver en ul//
+container.appendChild(ul); //sætter min ul ind i min container/appender til containeren//
+
+button.addEventListener("click", () => { //tilføjer eventlistener til min button//
+    const itemText = input.value; //får værdien af inputfeltet//
+    if (itemText.trim() !== " " ){ //tjekker om value er tom//
+        const li = document.createElement("li"); //skaber nyt li element//
+        const textNode = document.createTextNode(itemText); //skaber textNode//
+        li.appendChild(textNode); //appender textNode til li elementet//
+        ul.appendChild(li); //appender det nye li element til listen (ul)//
+        input.value = (""); //sørger for at input feltet bliver tomt//
+    }
+});
+
+function toggleChecked() {
+    var li = document.querySelector("li");
+    li.classList.toggle("checked");
+  }
+
