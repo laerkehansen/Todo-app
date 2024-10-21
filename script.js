5ded04d0e8eb9a372ef0f826cb7e6053df6a8a73
kati0234 <katinkaasp@gmail.com>
2024-10-21 12:40:37 +0200

    ik lav liste hvis fælt er tomt

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 
 let myArray = [];
 
-
 //henter alle mine elementer//
 const button = document.querySelector("button");
 const input = document.querySelector("input");
@@ -11,19 +10,24 @@ const container = document.getElementById("list-container");
 const ul = document.createElement("ul"); //laver en ul//
 container.appendChild(ul); //sætter min ul ind i min container/appender til containeren//
 
-button.addEventListener("click", () => { //tilføjer eventlistener til min button//
-    const itemText = input.value; //får værdien af inputfeltet//
-    if (itemText.trim() !== " " ){ //tjekker om value er tom//
-        const li = document.createElement("li"); //skaber nyt li element//
-        const textNode = document.createTextNode(itemText); //skaber textNode//
-        li.appendChild(textNode); //appender textNode til li elementet//
-        ul.appendChild(li); //appender det nye li element til listen (ul)//
-        input.value = (""); //sørger for at input feltet bliver tomt//
-    }
+button.addEventListener("click", () => {
+  //tilføjer eventlistener til min button//
+  const itemText = input.value; //får værdien af inputfeltet//
+
+  // if (itemText.trim() !== " " ){ //tjekker om value er tom//
+  if (itemText === "") {
+    // Tjekker om inputfeltet er tomt
+    // errorMessage.style.color = "red"; // Gør "Add task"-teksten rød
+  } else {
+    const li = document.createElement("li"); //skaber nyt li element//
+    const textNode = document.createTextNode(itemText); //skaber textNode//
+    li.appendChild(textNode); //appender textNode til li elementet//
+    ul.appendChild(li); //appender det nye li element til listen (ul)//
+    input.value = ""; //sørger for at input feltet bliver tomt//
+  }
 });
 
 function toggleChecked() {
-    var li = document.querySelector("li");
-    li.classList.toggle("checked");
-  }
-
+  var li = document.querySelector("li");
+  li.classList.toggle("checked");
+}
